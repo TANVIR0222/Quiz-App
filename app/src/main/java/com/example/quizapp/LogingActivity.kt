668898e6a89c25d43802ab89d1dc62475c252275
mac <tanvirislam3912@gmail.com>
2023-11-04 17:03:42 +0600

    first commet

---
+++ app/src/main/java/com/example/quizapp/LogingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loging)
+    }
+    
+}
